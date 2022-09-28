901eeb094f298f9f3623a555c8e7939ee27657de
pizzapup_ <hardwickariella@gmail.com>
2022-09-28 16:16:30 -0400

    updated styles

--- app.js
+++ app.js
@@ -9,50 +9,64 @@ fetch("../gamesData.json")
     console.log(err);
   });
 
-function loadData(GAMES) {
-  $.each(GAMES, function (idx, game) {
-    $("#app").append(
+function loadData(DATA) {
+  $("#app").html(
+    `<h2>Available Games</h2><div id="card-gallery" class='card-gallery'></div>`
+  );
+  $.each(DATA, function (idx, data) {
+    $("#card-gallery").append(
       `
-        <div id="${idx}" class="game-holder">
-        <h4>${game.gameTitle}</h4>
-        <div class="game-image">
-          <img src="images/games_thumb/${game.gameThumbImg}" alt="${game.gameTitle}" />
-        </div>
-        <div class="brief-rating">
-          <div class="brief-des">
-            ${game.gameBriefDescription}
+        <div id="${idx}" class="card">
+        <h3 class="card__title">${data.title}</h3>
+          <img src="images/games_thumb/${data.thumbImg}" alt="${data.title}" class='card__media'/>
+        <div class="card__info">
+          <div class="card__info-desc">
+            ${data.briefDescription}
           </div>
-          <div class="rating">Rating: ${game.gameRating}</div>
+            <div class="card__info-rating">
+              <span class='card__info-rating-tag'>Rating: </span>
+              <span class='card__info-rating-data'>${data.rating}</span>
+            </div>
+            <div class="card__info-price">
+              <span class='card__info-rating-tag'>Price: </span>
+              <span class='card__info-price-data'>${data.price}</span>
+            </div>
         </div>
       </div>
       `
     );
   });
-  initListeners(GAMES);
+  initListeners(DATA);
 }
 
-function initListeners(GAMES) {
-  $(".game-holder").click(function (e) {
-    let campingIndex = e.currentTarget.id;
-    $("#app").html(`
-          <div class="camping">
-        <h4>${GAMES[campingIndex].gameTitle}</h4>
-        <div class="campingImg">
-          <img src="images/${GAMES[campingIndex].gameFullImg}" alt="crash" />
+function initListeners(DATA) {
+  $(".card").click(function (e) {
+    let index = e.currentTarget.id;
+    $("#app").html(
+      `<h2>Product Info</h2><div id='product-page' class='product-page'></div>`
+    );
+    $("#product-page").html(`
+    
+          <div class="product">
+        <h3>${DATA[index].title}</h3>
+        <div class="product__media">
+          <img src="images/${DATA[index].fullImg}" alt="crash" class="product__media-img" />
         </div>
-        <div class="campingDes">
-        ${GAMES[campingIndex].gameDescription}
+        <div class="product__info-desc">
+        ${DATA[index].description}
         </div>
-        <div class="price">Price: ${GAMES[campingIndex].gameRating}</div>
-      </div><button class="close">Back</button>`);
-    addCloseListener(GAMES);
+        <div class="product__info-price"><span class='product__info-price-tag'>Price:</span> ${DATA[index].price}</div>
+        <div class="product__info-rating"><span class='product__info-rating-tag'>Rating:</span> ${DATA[index].rating}</div>
+      </div><button class="close btn">Go Back to Available Games</button>
+       `);
+    addCloseListener(DATA);
   });
 }
 
-function addCloseListener(GAMES) {
+function addCloseListener(DATA) {
   $(".close").click(function () {
     $("#app").html("");
-    loadData(GAMES);
+    loadData(DATA);
   });
 }
 //
